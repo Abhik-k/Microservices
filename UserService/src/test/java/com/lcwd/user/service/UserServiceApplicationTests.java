afc1cac5af18e75ca7d6893fb050261275bec4fd
Abhik-k <112130293+Abhik-k@users.noreply.github.com>
2024-04-27 02:42:57 +0530

    Creating Post, Put, Delete using Feign Client.

--- UserService/src/test/java/com/lcwd/user/service/UserServiceApplicationTests.java
+++ UserService/src/test/java/com/lcwd/user/service/UserServiceApplicationTests.java
@@ -1,7 +1,11 @@
 package com.lcwd.user.service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.lcwd.user.service.enteties.Rating;
+import com.lcwd.user.service.services.external.RatingService;
 
 @SpringBootTest
 class UserServiceApplicationTests {
@@ -9,5 +13,14 @@ class UserServiceApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
+	
+	@Autowired
+	RatingService ratingService;
+	
+//	@Test
+//	void createRating() {
+//		Rating rating = Rating.builder().rating(14).userId("").hotelId("").feedback("This is created using Feign Client").build();
+//		Rating newRating = ratingService.createRating(rating);
+//		System.out.println(newRating);
+//	}
 }
